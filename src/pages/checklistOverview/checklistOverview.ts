5e0300a6de29df4b24d412c7e9eaf63be58c666e
mnshaw <mnshaw@andrew.cmu.edu>
2018-03-27 21:42:13 -0400

    Clean checklist code, make value of checklist items a dictionary, and improve due date formatting

--- src/pages/checklistOverview/checklistOverview.ts
+++ src/pages/checklistOverview/checklistOverview.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { Storage } from '@ionic/storage';
 import { NewChecklistItemPage } from '../newChecklistItem/newChecklistItem';
 
@@ -13,61 +12,18 @@ export class ChecklistOverviewPage {
   tasks: any = [];
   newChecklistItemPage: any;
 
-  constructor(public navCtrl: NavController, private sqlite: SQLite, public storage: Storage) {
-    //this.getData();
+  constructor(public navCtrl: NavController, public storage: Storage) {
+    // storage.clear();
     this.loadTaskStorage();
     this.newChecklistItemPage = NewChecklistItemPage;
-  }
-
-  updateCheckbox() {
-    this.storage.set('name', 'Max');
-    this.storage.get('name').then((val) => {
-      console.log('Your name is', val);
-    });
   }
 
   loadTaskStorage(){
     console.log("load");
     this.tasks = [];
     this.storage.forEach( (value, key, index) => {
-      //console.log("This is the value", value)
-      //console.log("from the key", key)
-      //console.log("Index is" index)
-      this.tasks.push({key:key, value:value})
+      this.tasks.push({key:key, value:value});
     })
-
-  }
-
-  getData() {
-    this.sqlite.create({
-      name: 'ionicdb.db',
-      location: 'default'
-    }).then((db: SQLiteObject) => {
-      db.executeSql('CREATE TABLE IF NOT EXISTS tasklist (rowid INTEGER PRIMARY KEY, task VARCHAR(255), completed BIT)', {})
-      .then(res => console.log('Executed create table SQL'))
-      .catch(e => console.log(e));
-      db.executeSql('SELECT * FROM tasklist ORDER BY rowid DESC', {})
-      .then(res => {
-        this.tasks = [];
-        for(var i=0; i<res.rows.length; i++) {
-          this.tasks.push({
-            rowid:res.rows.item(i).rowid,
-            task:res.rows.item(i).task,
-            completed:res.rows.item(i).completed
-          })
-        }
-      }).catch(e => console.log(e));
-    }).catch(e => console.log(e));
-  }
-
-  addTask() {
-    this.sqlite.create({
-      name: 'ionicdb.db',
-      location: 'default'
-    }).then((db: SQLiteObject) => {
-      db.executeSql('INSERT INTO tasklist VALUES(NULL,"get Food", 0)',{})
-        .then(res => console.log(res))
-        .catch(e => console.log(e));
-    }).catch(e => console.log(e));
+    console.log(this.tasks);
   }
 }
--- src/pages/newChecklistItem/newChecklistItem.ts
+++ src/pages/newChecklistItem/newChecklistItem.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 import { Storage } from '@ionic/storage';
 import { ChecklistOverviewPage } from '../checklistOverview/checklistOverview';
   
@@ -10,62 +9,20 @@ import { ChecklistOverviewPage } from '../checklistOverview/checklistOverview';
 })
 export class NewChecklistItemPage {
 
-  tasks: any = [];
   taskName: string = '';
-  dueDate: Date = new Date();
+  category: string = '';
+  dueDate: string = new Date().toISOString();
+  taskNotes: string = '';
   checklistOverviewPage: any;
 
-  constructor(public navCtrl: NavController, private sqlite: SQLite, public storage: Storage) {
-    //this.getData();
+  constructor(public navCtrl: NavController, public storage: Storage) {
     this.checklistOverviewPage = ChecklistOverviewPage;
-  }
-
-  updateCheckbox() {
-    this.storage.set('name', 'Max');
-    this.storage.get('name').then((val) => {
-      console.log('Your name is', val);
-    });
   }
 
   addTaskStorage(){
     var key = this.taskName;
-    var value = this.dueDate;
+    var value = {"category":this.category, "dueDate":this.dueDate, "taskNotes":this.taskNotes};
     this.storage.set(key,value);
-    this.tasks.push({key:key,value:value});
-    console.log('add test test 1');
   }
 
-
-  getData() {
-    this.sqlite.create({
-      name: 'ionicdb.db',
-      location: 'default'
-    }).then((db: SQLiteObject) => {
-      db.executeSql('CREATE TABLE IF NOT EXISTS tasklist (rowid INTEGER PRIMARY KEY, task VARCHAR(255), completed BIT)', {})
-      .then(res => console.log('Executed create table SQL'))
-      .catch(e => console.log(e));
-      db.executeSql('SELECT * FROM tasklist ORDER BY rowid DESC', {})
-      .then(res => {
-        this.tasks = [];
-        for(var i=0; i<res.rows.length; i++) {
-          this.tasks.push({
-            rowid:res.rows.item(i).rowid,
-            task:res.rows.item(i).task,
-            completed:res.rows.item(i).completed
-          })
-        }
-      }).catch(e => console.log(e));
-    }).catch(e => console.log(e));
-  }
-
-  addTask() {
-    this.sqlite.create({
-      name: 'ionicdb.db',
-      location: 'default'
-    }).then((db: SQLiteObject) => {
-      db.executeSql('INSERT INTO tasklist VALUES(NULL,"get Food", 0)',{})
-        .then(res => console.log(res))
-        .catch(e => console.log(e));
-    }).catch(e => console.log(e));
-  }
 }
